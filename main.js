36063716065ffeef9d7459d62e8178f28687de33
Juan Pesce <juan.ignacio.pesce@gmail.com>
2024-02-25 15:36:17 -0300

    Segunda entrega

--- main.js
+++ main.js
@@ -1,17 +1,31 @@
-// //Entrega Entrega-ECMAScript-y-ECMAScript-avanzado Juan Pesce
+// Entrega Juan Pesce
+
+const { error } = require("console");
+const fs = require("fs");
 
 class ProductManager{
     
-    constructor(products){
+    constructor(){
+        this.path = "./fsActivity";
+        try{
+            const base = fs.readFileSync(this.path,'utf8');
+            this.products=json.parse(base);
 
-        this.products = [];
+        }catch (error){
 
+            this.products = [];
+        }
+
+        fs.writeFileSync(`${this.path}/products.txt`, JSON.stringify(this.products, null, '\t'));
     }
 
     static id = 0;
 
-    addProduct(title, description, price, thumbnail, code, stock){
+    async addProduct(title, description, price, thumbnail, code, stock){
+    
         
+    // Verifico que el producto cumpla los requisitos minimos
+
         if(!title || !description || !price || !thumbnail || !code || !stock){
             console.log("Error! Todos los campos son obligatorios.");
             return
@@ -24,21 +38,115 @@ class ProductManager{
 
         ProductManager.id ++    // Esto haria que el incremental sea automatico
 
-        this.products.push({title, description, price, thumbnail, code, stock, id:ProductManager.id});
+        const newProduct = {
+            id:ProductManager.id,
+            title,
+            description,
+            price,
+            thumbnail,
+            code,
+            stock,
+        };
+
+
+        this.products.push(newProduct);
+        console.log("Nuevo producto agregado: ", newProduct)
+
+        await fs.promises.writeFile(`${this.path}/products.txt`, JSON.stringify(this.products, null, "\t"))
+
     }
 
-    getProduct(){
-        return this.products;
+    async getProduct(){
+        // return this.products;
+
+        try {
+            const readFile = await fs.promises.readFile(`${this.path}/this.products.txt`, "utf8");
+            const productObject = json.parse(readFile)
+
+            return productObject;
+        }catch{
+            console.error("Error en la obtencion del producto", error);
+        }
+        
     }
     
-    getProductById(id) {
-		if(this.products.some(prod => prod.id === id)) {
-			return this.products.find(prod => prod.id === id);
-		} else {
-			console.log('Not found');
-		}
+    async getProductById(id) {
+        try{
+
+            const productID = parseInt(id);
+            const productList = this.products.some(prod => prod.id === productID);
+
+            if(productList){
+                return productList;
+            } else {
+                console.error("Not found.");
+            }
+            
+        } catch(error){
+            console.error("Error en la obtencion del ID", error);
+            return;
+        }
+        
     }
 
+    async updateProduct(id, { title, description, price, thumbnail, code, stock }){
+        // try{
+        //     const reachID = this.products.findIndex(product => product.id === id);
+        //     if(reachID >0){
+
+        //         this.products[reachID] = { ...this.products[reachID], ...base, id: id };
+        //         fs.writeFileSync(`${this.path}/products.txt`, JSON.stringify(this.products, null, '\t'));
+
+        //     }else{
+        //         console.error("Not Found for update")
+        //     }
+
+        // }catch(error){
+        //       console.error("Error en el Update")
+
+        // }
+
+        {
+            const findId = this.products.findIndex((product) => product.id === id);
+            if (findId === -1) return console.error("No encontrado");
+        
+            const updateProduct = {
+              ...this.products[findId],
+              title,
+              description,
+              price,
+              thumbnail,
+              code,
+              stock,
+            };
+        
+            this.products[findId] = updateProduct;
+        
+            try {
+            await fs.promises.writeFile(`${this.path}/products.txt` , JSON.stringify(this.products , null , "\t"));
+
+            } catch (error) {
+              console.error("Error escribiendo archivo", error);
+            }
+          }
+
+
+    }
+
+    async deleteProduct(id){
+        const deleteForId = this.products.findIndex((product) => product.id === id);
+        if (deleteForId === -1) {
+          console.error("Not found");
+          return;
+        }
+        this.products.splice(deleteForId, 1);
+    
+
+        await fs.promises.writeFile(`${this.path}/products.txt` , JSON.stringify(this.products , null , "\t"));
+
+        console.log(`El producto con el id ${id} fue borrado correctamente`);
+
+    }
 
 
 }
@@ -62,4 +170,16 @@ console.log(Products.getProduct());
 console.log(Products.getProductById(2)); // Búsqueda por Id
 console.log(Products.getProductById(5)); // Búsqueda por Id not found
 
+
+
+Products.updateProduct(1, {
+    title: "Yerba Saborizada",
+    description: "Yerba con naranja",
+    price: 3800,
+    thumbnail: "yerba1.jpg",
+    code: "QWE123",
+    stock: 300,
+  });
+
+  Products.deleteProduct(3);
 
